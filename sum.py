e58610cdcd8dd73725f2e96cdc7c8fcb38c7616c
AndyFeiLi <59521017+AndyFeiLi@users.noreply.github.com>
2020-11-19 12:43:11 +0800

    Update sum.py

--- sum.py
+++ sum.py
@@ -2,7 +2,7 @@ import xlsxwriter
 
 # Create a workbook and add a worksheet.
 workbook = xlsxwriter.Workbook('VM.xlsx')
-worksheet = workbook.add_worksheet()
+worksheet = workbook.add_worksheet("VM")
 
 # Start from the first cell. Rows and columns are zero indexed.
 row = 0
@@ -66,45 +66,137 @@ with open('myoutput.txt') as f:
           
            currentCompanyName = splitLineName
            
+           ########hard coded hacky stuff here - maintain this or rename VMs#########
+           
+           if(currentCompanyName == "riverstone"):
+               currentCompanyName = "rsc"
+           elif(currentCompanyName == "rscapp01"):
+               currentCompanyName = "rsc"
+           elif(currentCompanyName == "rscdc01"):
+               currentCompanyName = "rsc"
+           elif(currentCompanyName == "tbcwestperth"):
+               currentCompanyName = "tbcwp"
+           elif(currentCompanyName == "htx2"):
+               currentCompanyName = "htx"
+           elif(currentCompanyName == "nbc"):
+               currentCompanyName = "northfleet"
+           elif(currentCompanyName == "mgmt"):
+               currentCompanyName = "cloudconnect"
+           elif(currentCompanyName == "vm"):
+               currentCompanyName = "cloudconnect"
+           elif(currentCompanyName == "mgmt"):
+               currentCompanyName = "cloudconnect"
+           elif(currentCompanyName == "3cx"):
+               currentCompanyName = "cloudconnect"
+           elif(currentCompanyName == "cloudcore"):
+               currentCompanyName = "cloudconnect"
+           elif(currentCompanyName == "radius"):
+               currentCompanyName = "cloudconnect"
+           elif(currentCompanyName == "vm"):
+               currentCompanyName = "cloudconnect"
+           elif(currentCompanyName == "p1"):
+               currentCompanyName = "cloudconnect"
+           
+           ############################################
+           
            row = row + 1
            
            #write worksheet
            worksheet.write(row, 0, line)
            
-           if splitLineName not in my_dict.keys():
-               my_dict[splitLineName] = "empty"
+           if currentCompanyName not in my_dict.keys():
+               my_dict[currentCompanyName] = "empty"
                
        
        if 'str' in line:
           break
 
-workbook.close()          
+#workbook.close()          
           
           
-workbook = xlsxwriter.Workbook('Totals.xlsx')
-worksheet = workbook.add_worksheet()
+#workbook = xlsxwriter.Workbook('Totals.xlsx')
+worksheet1 = workbook.add_worksheet("Totals")
 
 # Start from the first cell. Rows and columns are zero indexed.
 row = 0
 col = 0
 
-worksheet.write(0, 0, 'Name')
-worksheet.write(0, 1, 'CPU')
-worksheet.write(0, 2, 'SSD')
-worksheet.write(0, 3, 'RAM')
+worksheet1.write(0, 0, 'Name')
+worksheet1.write(0, 1, 'CPU')
+worksheet1.write(0, 2, 'SSD')
+worksheet1.write(0, 3, 'RAM')
 
 for key in my_dict:
     row = row + 1
     #print(key)
-    worksheet.write(row, 0, key)
+    
+    ########hard coded hacky stuff here - maintain this or rename VMs#########
+           
+    name = "default"
+    
+    if(key == "a2w"):
+        name = "Aspire2 Wealth"
+    elif(key == "acc"):
+        name = "Access Solutions"
+    elif(key == "cathedral"):
+        name = "Cathedral Office Products"
+    elif(key == "htx"):
+        name = "Haultrax"
+    elif(key == "jwms"):
+        name = "Jetwave Marine Services"               
+    elif(key == "kais"):
+        name = "Kais Contractors"            
+    elif(key == "pmm"):
+        name = "PMM Wealth Advisors"             
+    elif(key == "stk"):
+        name = "Stickman Tribe"        
+    elif(key == "tbcwp"):
+        name = "Patter Merchants"             
+    elif(key == "kng"):
+        name = "Kongsberg Maritime"   
+    elif(key == "sleep"):
+        name = "Sleep Studies"        
+    elif(key == "sleep"):
+        name = "Sleep Studies"        
+    elif(key == "rsc"):
+        name = "Riverstone Custom Homes"  
+    elif(key == "firstnational"):
+        name = "First National Real Estate"
+    elif(key == "northfleet"):
+        name = "Northfleet Bus Contractors"  
+    elif(key == "plr"):
+        name = "Polaris Realty"    
+    elif(key == "pme"):
+        name = "Plantman Equipment"    
+    elif(key == "efw"):
+        name = "European Foods Wholesalers"        
+    elif(key == "elev8"):
+        name = "Insphire Australia"  
+    elif(key == "get.trakka"):
+        name = "GetTrakka"
+    elif(key == "hiresociety"):
+        name = "Hire Society"
+    elif(key == "cloudconnect"):
+        name = "Cloud Connect"
+    elif(key == "tax"):
+        name = "Taxcorp"
+    elif(key == "pba"):
+        name = "Pilbara Airlines"
+        
+   ############################################ 
+    
+    if(name == "default"):
+        worksheet1.write(row, 0, key)
+    else:
+        worksheet1.write(row, 0, name)
     
     for innerkey in my_dict[key]:
         if(innerkey == "CPU:"):
-            worksheet.write(row, 1, my_dict[key][innerkey])
+            worksheet1.write(row, 1, my_dict[key][innerkey])
         if(innerkey == "SSD:"):
-            worksheet.write(row, 2, my_dict[key][innerkey])
+            worksheet1.write(row, 2, my_dict[key][innerkey])
         if(innerkey == "RAM:"):
-            worksheet.write(row, 3, my_dict[key][innerkey])
+            worksheet1.write(row, 3, my_dict[key][innerkey])
                
         #print(innerkey + " ", end = '')
         #print(my_dict[key][innerkey])
