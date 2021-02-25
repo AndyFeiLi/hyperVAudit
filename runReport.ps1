dda0d87e0ffdf08d71ff8d3723d244ec49d615d1
AndyFeiLi <59521017+AndyFeiLi@users.noreply.github.com>
2021-02-25 12:02:04 +0800

    Update runReport.ps1

--- runReport.ps1
+++ runReport.ps1
@@ -1,3 +1,4 @@
 Remove-Item -Path .\myoutput.txt
-.\Get-HyperVReport.ps1 -Cluster P1-CL01
+#.\Get-HyperVReport.ps1 -Cluster P1-CL01
+.\Get-HyperVReport.ps1 -VMHost S2D-SERVER01,S2D-SERVER06
 python sum.py
